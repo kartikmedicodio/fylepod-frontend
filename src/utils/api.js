63ba1fe69d691f28d71e01c8d9b3a420579ef570
kartikmedicodio <kartik.b@MediCodio.com>
2025-01-24 11:33:37 +0530

    app-dev changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.relayzen.com/api",
+  baseURL: "https://api-dev.relayzen.com/api",
   headers: {
     "Content-Type": "application/json",
   },
